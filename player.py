a3308590889cb38895b1909c2fea4b0fe360a7b8
Andrew <andrewkoltsov@gmail.com>
2015-08-01 10:57:12 +0600

    preflop

--- player.py
+++ player.py
@@ -6,11 +6,25 @@ pp = pprint.PrettyPrinter(indent=4)
 class Player:
     VERSION = "Inky 0.1"
 
+    def pre_flop_power1(self, hand):
+        if hand.rank == 1 and hand.value >= 10:
+            return True
+        if hand.rank == 0 and hand.value >= 14 and hand.value2 >= 10:
+            return True
+        return False
+
+    def pre_flop_power3(self, hand):
+        if hand.rank == 1 and hand.value >= 10:
+            return True
+        if hand.rank == 0 and hand.value >= 14 and hand.value2 >= 10:
+            return True
+        return False
+
     def betRequest(self, game_state):
         print pp.pprint(game_state)
         if not 'in_action' in game_state.keys():
             return 0
-
+        self.game_state = game_state
         me = game_state['players'][game_state['in_action']]
         call = game_state['current_buy_in'] - me['bet']
         my_cards = me['hole_cards']
@@ -18,15 +32,13 @@ class Player:
         if len(community_cards) == 0:
             # pre flop
             hand = Hand(my_cards)
-            if hand.rank > 1:
+            if self.pre_flop_power1(hand):
                 return me['stack']
             if hand.rank == 1:
-
-            pass
+                pass
         else:
             cards = my_cards + community_cards
-            pass
-
+            hand = Hand(my_cards)
 
         return me['stack']
 
